fcc7069d517a94db2e8822527e4b7c4bd14fcbd0
MocoAristo <y.s.11921120@gmail.com>
2021-10-12 09:51:12 +0900

    fix: file

--- test.rb
+++ test.rb
@@ -1,8 +1,1 @@
-p "a b c d".split()
-p "a\nb\nc\nd".split(//)
-p "a\tb\tc\td".split()
-p "a b c d".split(//)
-a = /a/
-p a.class
-hash = Hash[:a,1,"hash" => 2]
-p hash
+p [1,2,3,4].map do |e| e * e end
